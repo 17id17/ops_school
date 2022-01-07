0973c7bf772923b75d290f58a2e72227cfe467e6
17id17 <97056750+17id17@users.noreply.github.com>
2022-01-08 00:32:39 +0200

    Update exercise5-fix_server1.sh

--- materials/vagrant/fixScripts/exercise5-fix_server1.sh
+++ materials/vagrant/fixScripts/exercise5-fix_server1.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
 #add fix to exercise5-server1 here
+sudo sed -i '$ a 192.168.100.11 server2 server2' /etc/hosts
+sudo sed -i '$ a StrictHostKeyChecking no' /etc/ssh/ssh_config
+sudo sed -i 's/PubkeyAuthentication yes/#PubkeyAuthentication yes/g' /etc/ssh/sshd_config
+sudo service sshd restart
+ssh-keygen -q -t rsa -N '' -f ~/.ssh/id_rsa_server1
+sudo cp /home/vagrant/.ssh/id_rsa_server1.pub /vagrant
+echo "Run script for server2"
+sleep 30  
+echo "all Done"
+sudo cat /vagrant/id_rsa_server2.pub >> ~/.ssh/authorized_keys
