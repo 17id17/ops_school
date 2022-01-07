285fe76350ce55fa4358c4acca41ce113c53ce22
17id17 <97056750+17id17@users.noreply.github.com>
2022-01-07 12:24:50 +0200

    Update exercise4-fix_server2.sh

--- materials/vagrant/fixScripts/exercise4-fix_server2.sh
+++ materials/vagrant/fixScripts/exercise4-fix_server2.sh
@@ -2,4 +2,4 @@
 #add fix to exercise4-server2 here
 sudo sed -i '$ a StrictHostKeyChecking no' /etc/ssh/ssh_config
 sudo sed -i '$ a 192.168.100.10 server1 server1' /etc/hosts
-sudo ssh server1
+sudo ssh server2
