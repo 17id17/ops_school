7fe336ce0e845e800d3aa32194bae9a52ce932a4
17id17 <97056750+17id17@users.noreply.github.com>
2022-01-13 15:01:53 +0200

    Update exercise5-fix_server2.sh

--- materials/vagrant/fixScripts/exercise5-fix_server2.sh
+++ materials/vagrant/fixScripts/exercise5-fix_server2.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 #add fix to exercise5-server2 here
-sudo sed -i 's/#PubkeyAuthentication yes/PubkeyAuthentication yes/g' /etc/ssh/sshd_config
-sudo service sshd restart
-ssh-keygen -q -t rsa -N '' -f ~/.ssh/id_rsa2 <<<y >/dev/null 2>&1
+# adding key of server1 to authosized key of server2
+sudo cat /vagrant/id_rsa.pub >> ~/.ssh/authorized_keys
+
+#sudo sed -i 's/#PubkeyAuthentication yes/PubkeyAuthentication yes/g' /etc/ssh/sshd_config
+#sudo service sshd restart
+
+#key creation on server 2
+ssh-keygen -q -t rsa -N '' -f ~/.ssh/id_rsa <<<y >/dev/null 2>&1
+#copy key of server2 to vagrant
 sudo cp /home/vagrant/.ssh/id_rsa2.pub /vagrant
-sudo cat /vagrant/id_rsa1.pub >> ~/.ssh/authorized_keys
-ssh server1
+
+
