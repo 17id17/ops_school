e4b2dc16b7239899c8e1bb3360bfa95a1414643e
17id17 <97056750+17id17@users.noreply.github.com>
2022-01-13 15:57:29 +0200

    Update exercise5-fix_server1.sh

--- materials/vagrant/fixScripts/exercise5-fix_server1.sh
+++ materials/vagrant/fixScripts/exercise5-fix_server1.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 #add fix to exercise5-server1 here
-#sudo sed -i 's/#PubkeyAuthentication yes/PubkeyAuthentication yes/g' /etc/ssh/sshd_config
-#sudo service sshd restart
+sudo sed -i '$ a 192.168.100.11 server2 server2' /etc/hosts
+sudo sed -i '$ a StrictHostKeyChecking no' /etc/ssh/ssh_config
+sudo sed -i 's/#PubkeyAuthentication yes/PubkeyAuthentication yes/g' /etc/ssh/sshd_config
+sudo service sshd restart
+
 #keys creation server1
 ssh-keygen -q -t rsa -N '' -f ~/.ssh/id_rsa <<<y >/dev/null 2>&1
+
 #copy key to vagrant 
 sudo cp /home/vagrant/.ssh/id_rsa.pub /vagrant
-echo "Sleep of 2 minutes to complete running of execise5-fix_server2.sh or you can use ctrl+c to exit"
+echo "Sleep of 1 minutes to complete running of execise5-fix_server2.sh or you can use ctrl+c to exit"
 sleep 2m  
-echo "Sleep completed"
+echo "Sleep completed, updating autorize key of server1"
 #run folloing command on server1 
 sudo cat /vagrant/id_rsa.pub >> ~/.ssh/authorized_keys
-
-ssh server2
-exit
-ssh server1
