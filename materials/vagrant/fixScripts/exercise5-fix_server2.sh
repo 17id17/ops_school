1707f30de5a3296bbebe4a0cd2df96235f5cda90
17id17 <97056750+17id17@users.noreply.github.com>
2022-01-13 15:11:48 +0200

    Update exercise5-fix_server2.sh

--- materials/vagrant/fixScripts/exercise5-fix_server2.sh
+++ materials/vagrant/fixScripts/exercise5-fix_server2.sh
@@ -9,6 +9,6 @@ sudo cat /vagrant/id_rsa.pub >> ~/.ssh/authorized_keys
 #key creation on server 2
 ssh-keygen -q -t rsa -N '' -f ~/.ssh/id_rsa <<<y >/dev/null 2>&1
 #copy key of server2 to vagrant
-sudo cp /home/vagrant/.ssh/id_rsa2.pub /vagrant
+sudo cp /home/vagrant/.ssh/id_rsa.pub /vagrant
 
 
